638b7654cc6864fe1e75b2d6304e0834075d92c7
Konstantin <the.it.promo@gmail.com>
2023-05-12 00:12:07 +0600

    add button and head text

--- lib/screens/home_scrren.dart
+++ lib/screens/home_scrren.dart
@@ -9,17 +9,65 @@ class HomeScreen extends StatelessWidget {
   static const routeName = '/';
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
-          preferredSize: Size.fromHeight(70),
-          child: SafeArea(
-            child: Header(),
-          ),
+    return Scaffold(
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(70),
+        child: SafeArea(
+          child: Header(),
         ),
-      bottomNavigationBar: BottomNavBar(
+      ),
+      bottomNavigationBar: const BottomNavBar(
         index: 0,
       ),
-      body: Text('Content'),
+      body: Container(
+        padding: const EdgeInsets.only(left: 10, right: 10, top: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color.fromARGB(255, 235, 235, 235),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                elevation: 0.0,
+                shadowColor: Colors.transparent,
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  const Text(
+                    'Фильтры',
+                    style: TextStyle(
+                        fontSize: 14, color: Color.fromRGBO(78, 78, 78, 1)),
+                  ),
+                  const SizedBox(
+                    width: 6,
+                  ),
+                  Image.asset(
+                    'assets/icons/filterIcon.png',
+                    width: 18,
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Букеты-бестселлеры',
+              style: TextStyle(
+                  color: Color.fromRGBO(53, 53, 53, 1),
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
